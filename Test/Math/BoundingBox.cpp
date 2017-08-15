80b161101065a9835186d53e1414e226d3be5c78
newpolaris <newpolaris@gmail.com>
2017-08-15 17:40:57 +0900

    Add bounding sphere test (to verify frustum error)

--- Test/Math/BoundingBox.cpp
+++ Test/Math/BoundingBox.cpp
@@ -18,3 +18,17 @@ TEST(BoundingBoxTest, OrthogonalTransform)
     result = Matrix4::MakeScale( Vector3( 2.f ) ) * box;
     EXPECT_THAT( result.GetMin(), MatcherNearFast( 1e-5f, Vector3( -10.f ) ) );
 }
+
+TEST(BoundingBoxTest, FrustumCorner)
+{
+    float Left = -1.f, Right = 1.f, Bottom = -1.f, Top = 1.f, Near = 0.1f, Far = 10000.f;
+    Vector3 min( Left, Bottom, Near ), max( Right, Top, Far );
+
+    Matrix4 Proj = OrthographicMatrix( Left, Right, Bottom, Top, Near, Far, false );
+    Frustum frustum( Proj );
+    BoundingBox box( min, max );
+    auto corners = box.GetCorners();
+
+    EXPECT_THAT( frustum.GetFrustumCorners(), Pointwise( MatcherNearRelative( FLT_EPSILON ), corners) );
+}
+
--- Test/Math/Math.cpp
+++ Test/Math/Math.cpp
@@ -4,7 +4,6 @@
 #include "VectorMath.h"
 #include "Camera.h"
 #include "OrthographicCamera.h"
-
 #include "Math/BoundingBox.h"
 
 using namespace Math;
@@ -57,57 +56,6 @@ TEST(BoundingBoxTest, Corner)
 
     BoundingBox box( Vector3(Left, Bottom, Far), Vector3(Right, Top, Near));
     EXPECT_THAT( Corners, Pointwise( MatcherNearFast( FLT_EPSILON ), box.GetCorners() ) );
-}
-
-TEST(BoundingBoxTest, FrustumCorner)
-{
-    float Left = -1.f, Right = 1.f, Bottom = -1.f, Top = 1.f, Near = 0.1f, Far = 10000.f;
-    Vector3 min( Left, Bottom, Near ), max( Right, Top, Far );
-
-    Matrix4 Proj = OrthographicMatrix( Left, Right, Bottom, Top, Near, Far, false );
-    Frustum frustum( Proj );
-    BoundingBox box( min, max );
-    auto corners = box.GetCorners();
-
-    EXPECT_THAT( frustum.GetFrustumCorners(), Pointwise( MatcherNearRelative( FLT_EPSILON ), corners) );
-}
-
-TEST(FrustumTest, WorldSpaceCorner)
-{
-    Vector3 Pos( 10.f, -10.f, 10.f ), At( 0.f, 0.f, 5.f ), Up( kYUnitVector );
-
-    Camera cam;
-    cam.SetEyeAtUp( Pos, At, Up );
-    cam.Update();
-
-    Frustum FrustumVS( cam.GetProjMatrix() );
-    Frustum FrustumWS = cam.GetCameraToWorld() * FrustumVS;
-    auto Coners = cam.GetWorldSpaceFrustum().GetFrustumCorners();
-
-    EXPECT_THAT( FrustumWS.GetFrustumCorners(), Pointwise( MatcherNearRelative( FLT_EPSILON ), Coners) );
-}
-
-TEST(FrustumTest, WorldSpaceCorner2)
-{
-    Vector3 Pos( 0.f ), At( 1.f, 1.f, 1.f ), Up( kYUnitVector );
-
-    OrthographicCamera cam;
-    cam.SetEyeAtUp( Pos, Normalize(At), Up );
-    cam.Update();
-
-    Frustum FrustumVS( cam.GetProjMatrix() );
-    Frustum FrustumWS = cam.GetCameraToWorld() * FrustumVS;
-    auto Coners = cam.GetWorldSpaceFrustum().GetFrustumCorners();
-
-    EXPECT_THAT( FrustumWS.GetFrustumCorners(), Pointwise( MatcherNearRelative( FLT_EPSILON ), Coners) );
-
-    Matrix4 InvView = Invert(cam.GetViewMatrix());
-    FrustumCorner CornersWS;
-    auto CornersVS = FrustumVS.GetFrustumCorners();
-    for (auto i = 0; i < CornersVS.size(); i++)
-        CornersWS[i] = InvView.Transform( CornersVS[i] );
-
-    EXPECT_THAT( CornersWS, Pointwise( MatcherNearRelative( 1e-5f), Coners) );
 }
 
 TEST(CameraTest, LookAt)
