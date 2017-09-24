c717cbd09c47bb13a97ab37aa2f82e2c8c6faa2e
newpolaris <newpolaris@gmail.com>
2017-09-24 13:24:32 +0900

    Apply diffuse attenuation

--- Mikudayo/Shaders/PmxColorPS.hlsl
+++ Mikudayo/Shaders/PmxColorPS.hlsl
@@ -3,10 +3,10 @@
 // Per-pixel color data passed through the pixel shader.
 struct PixelShaderInput
 {
-	float4 posHS : SV_POSITION;
-	float3 posVS : POSITION0;
-	float3 normalVS : NORMAL;
-	float2 uv : TEXTURE;
+    float4 posHS : SV_POSITION;
+    float3 posVS : POSITION0;
+    float3 normalVS : NORMAL;
+    float2 uv : TEXTURE;
 };
 
 static const int kSphereNone = 0;
@@ -15,69 +15,67 @@ static const int kSphereAdd = 2;
 
 cbuffer MaterialConstants : register(b0)
 {
-	Material material;
-	int sphereOperation;
-	int bUseTexture;
-	int bUseToon;
+    Material material;
+    int sphereOperation;
+    int bUseTexture;
+    int bUseToon;
 };
 
 cbuffer PassConstants : register(b1)
 {
     float3 SunDirectionVS;
     float3 SunColor;
-    float4 ShadowTexelSize;
 }
 
 Texture2D<float4> texDiffuse : register(t1);
 Texture2D<float3> texSphere : register(t2);
 Texture2D<float3> texToon : register(t3);
-Texture2DArray<float> texShadow : register(t4);
 
 SamplerState sampler0 : register(s0);
 SamplerState sampler1 : register(s1);
-SamplerComparisonState samplerShadow : register(s2);
-
-static const float Bias = 0.f;
 
 // A pass-through function for the (interpolated) color data.
 float4 main(PixelShaderInput input) : SV_TARGET
 {
-	float3 lightVecVS = normalize( -SunDirectionVS );
-	float3 normalVS = normalize( input.normalVS );
-	float intensity = dot( lightVecVS, normalVS ) * 0.5 + 0.5;
-	float2 toonCoord = float2(0.5, 1.0 - intensity);
+    float3 lightVecVS = normalize(-SunDirectionVS);
+    float3 normalVS = normalize(input.normalVS);
+    float intensity = dot(lightVecVS, normalVS) * 0.5 + 0.5;
+    float2 toonCoord = float2(0.5, 1.0 - intensity);
 
-	float3 toEyeV = -input.posVS;
-	float3 halfV = normalize( toEyeV + lightVecVS );
+    float3 toEyeV = -input.posVS;
+    float3 halfV = normalize(toEyeV + lightVecVS);
 
-	float NdotH = dot( normalVS, halfV );
-	float specularFactor = pow( max(NdotH, 0.001f), material.specularPower );
+    float NdotH = dot(normalVS, halfV);
+    float specularFactor = pow(max(NdotH, 0.001f), material.specularPower);
+    float3 sunDiffuse = SunColor * max(0, dot(lightVecVS, normalVS));
+    float3 sunSpecular = SunColor * specularFactor;
 
-    LightingResult lit = DoLighting( Lights, material, input.posVS, input.normalVS );
-    lit.Diffuse.rgb += SunColor;
-    lit.Specular.rgb += specularFactor.xxx;
-    
-	float3 diffuse = material.diffuse * lit.Diffuse.rgb;
-	float3 ambient = material.ambient;
-	float3 specular = material.specular * lit.Specular.rgb;
+    LightingResult lit = DoLighting(Lights, material, input.posVS, input.normalVS);
 
-	float texAlpha = 1.0;
-	float3 texColor = float3(1.0, 1.0, 1.0);
-	if (bUseTexture)
-	{
-		float4 tex = texDiffuse.Sample( sampler0, input.uv );
-		texColor = tex.xyz;
-		texAlpha = tex.w;
-	}
+    lit.Diffuse.rgb += sunDiffuse;
+    lit.Specular.rgb += sunSpecular;
 
-	float2 sphereCoord = 0.5 + 0.5*float2(1.0, -1.0) * normalVS.xy;
-	if (sphereOperation == kSphereAdd)
-		texColor += texSphere.Sample( sampler0, sphereCoord );
-	else if (sphereOperation == kSphereMul)
-		texColor *= texSphere.Sample( sampler0, sphereCoord );
-	float3 color = texColor * (ambient + diffuse) + specular;
+    float3 diffuse = material.diffuse * lit.Diffuse.rgb;
+    float3 ambient = material.ambient;
+    float3 specular = material.specular * lit.Specular.rgb;
+
+    float texAlpha = 1.0;
+    float3 texColor = float3(1.0, 1.0, 1.0);
+    if (bUseTexture)
+    {
+        float4 tex = texDiffuse.Sample(sampler0, input.uv);
+        texColor = tex.xyz;
+        texAlpha = tex.w;
+    }
+
+    float2 sphereCoord = 0.5 + 0.5*float2(1.0, -1.0) * normalVS.xy;
+    if (sphereOperation == kSphereAdd)
+        texColor += texSphere.Sample(sampler0, sphereCoord);
+    else if (sphereOperation == kSphereMul)
+        texColor *= texSphere.Sample(sampler0, sphereCoord);
+    float3 color = texColor * (ambient + diffuse) + specular;
     if (bUseToon)
-        color *= texToon.Sample( sampler0, toonCoord );
-	float alpha = texAlpha * material.alpha;
-	return float4(color, alpha);
+        color *= texToon.Sample(sampler0, toonCoord);
+    float alpha = texAlpha * material.alpha;
+    return float4(color, alpha);
 }
