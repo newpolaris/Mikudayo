ae4c335635f04ad608e551f829d1de1b15ad56ae
newpolaris <newpolaris@gmail.com>
2017-11-18 23:10:56 +0900

    Fix skydome sampler

--- Mikudayo/Shaders/SkydomeColorPS.hlsl
+++ Mikudayo/Shaders/SkydomeColorPS.hlsl
@@ -1,10 +1,8 @@
 Texture2D<float4> texDiffuse : register(t1);
 
-SamplerState sampler0 : register(s0);
 SamplerState sampler1 : register(s1);
-SamplerState sampler3 : register(s3);
 
 float4 main( float2 texcoord : TexCoord ) : SV_Target
 {
-    return texDiffuse.Sample(sampler3, texcoord );
+    return texDiffuse.Sample(sampler1, texcoord );
 }
