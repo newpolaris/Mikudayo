afd8105bb6c2546ff8880b7c263cb55d437ce33a
newpolaris <newpolaris@gmail.com>
2017-11-20 12:42:18 +0900

    Add SSAO miniEngine version

---
+++ Core/RootSignature.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "RootSignature.h"
+
+void RootSignature::DestroyAll( void )
+{
+}
+
+void RootSignature::InitStaticSampler( UINT Register, const SamplerDesc& NonStaticSamplerDesc, D3D12_SHADER_VISIBILITY Visibility )
+{
+}
+
+void RootSignature::Finalize( const std::wstring & name, D3D12_ROOT_SIGNATURE_FLAGS Flags )
+{
+}
